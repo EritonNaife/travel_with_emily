bec84b03f38432904a91c12f59283eac6391bef6
Eriton <up202008859@up.pt>
2025-10-23 17:43:48 +0100

    added DOMPurify

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,15 @@
 // src/routes/.../+page.server.ts
 import type { PageServerLoad, Actions } from './$types';
 import { supabase } from '$lib/supabaseClient';
-import { error, fail } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
 import { tourInquiryEmail } from '$lib/email/templates';
 import { resend } from '$lib/email/resend';
 import { inquirySchema } from '$lib/zod/schema';
-import { da } from 'zod/locales';
+import DOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const window = new JSDOM('').window;
+const purify = DOMPurify(window);
 
 export const load: PageServerLoad = async () => {
 	const [tourRes, packagesRes] = await Promise.all([
@@ -35,14 +39,10 @@ export const actions: Actions = {
 
 		const { name, email, message, tour_id, phone_number, preferred_date } = result.data;
 
+		const sanitizedMessage = purify.sanitize(message);
 		try {
 			console.log('Received validated data:', result.data);
-			await resend.emails.send({
-				from: 'onboarding@resend.dev',
-				to: 'delivered@resend.dev',
-				subject: `New Tour Inquiry`,
-				html: tourInquiryEmail({ name, email, phone_number, preferred_date, message, tour_id })
-			});
+
 			return { success: true, message: 'Your inquiry has been submitted succefully!' };
 		} catch (error) {
 			console.error('Error:', error);
@@ -56,44 +56,12 @@ export const actions: Actions = {
 		// --- Optional: store inquiry in Supabase ---
 		/*
 
-		console.log('📩 Received inquiry:', {
-			
-
-			name,
-			email,
-			phone_number,
-			preferred_date,
-			message,
-			tour_id
-		});
-
-		try {
-			const { data, error: emailError } = await resend.emails.send({
+		await resend.emails.send({
 				from: 'onboarding@resend.dev',
-				to: 'delivered@resend.dev', 
+				to: 'delivered@resend.dev',
 				subject: `New Tour Inquiry`,
-				html: tourInquiryEmail({
-					name,
-					email,
-					phone_number,
-					preferred_date,
-					message,
-					tour_id
-				})
+				html: tourInquiryEmail({ name, email, phone_number, preferred_date, message, tour_id })
 			});
-
-			if (emailError) {
-				console.error('Resend email error:', emailError);
-				return fail(500, { error: 'Failed to send email. Please try again.' });
-			}
-
-			console.log('✅ Email sent via Resend:', data);
-			return { success: true, message: 'Your inquiry has been submitted!' };
-		} catch (err) {
-			console.error('Unexpected error sending email:', err);
-			return fail(500, { error: 'An unexpected error occurred while sending your inquiry.' });
-		}
-		
 		*/
 
 		return { success: true };
